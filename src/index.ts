bc80c1761f00984369ba6c77e8617e104d50d683
GregorisB <gregorbachts@gmail.com>
2023-03-12 01:17:32 +0200

    try functionalities

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,13 @@
-class Translate {
-    language: string;
-
-    constructor(languageCode:string){
-        this.language = languageCode
-    }
-    
-     lang(_string: string, _data: string | null){
-        let lang_strings
-        if(!Boolean(lang_strings[_string])){
-            return _string;
+const lang = (string: string, data?: string[] | number[]) => {
+    var test = 'this {#1} is {#2} a test {#3}'
+    if (data){
+        for(let i=0;i<=data.length;i++){
+            test = test.replace(`{#${i}}`, data[i-1])
         }
-        else{
-            if(_data == '')
-                return lang_strings[_string];
-            else {
-                return_string = lang_strings[_string];
-                if(!is_array(_data)){
-                    _data = array(_data);
-                }
-                i = 1;
-                foreach(_data as val){
-                    return_string = str_replace('{'.i.'}', val, return_string);
-                    i++;
-                }
-                return return_string;
-            }
-        }
-    }
-     
+       return  console.log(string, test)
 
+    }
+    return console.log(string)
 }
+
+lang('hello', [1,5,6])
---
+++ src/test.ts
@@ -0,0 +1,16 @@
+class Translate {
+    
+    constructor(){
+        
+    }
+    
+    lang(_string: string, data?: string | number){
+        console.log(_string)
+        
+    }
+     
+
+}
+
+let t = new Translate()
+t.lang('test123')
