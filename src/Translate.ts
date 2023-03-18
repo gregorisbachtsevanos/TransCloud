28e2bd9d3c23892bfc00ae9a07d5ecad0cf75d49
GregorisB <gregorbachts@gmail.com>
2023-03-18 13:39:47 +0200

    add lang switcher

--- src/Translate.ts
+++ src/Translate.ts
@@ -4,22 +4,42 @@ import { el } from "./lang/el"
 import { en } from "./lang/en"
 
 class Translate {
-    
-    constructor(){
+    lang: string
+
+    constructor(lang: string){
+        this.lang = lang
     }
 
-    lang = (_string: string, data?: any) => {
-        var value = en[_string as keyof typeof en]
+    private getCurrentlyLang(_string: string) {
+        let value
+        switch (this.lang) {
+            case 'el':
+                value = el[_string as keyof typeof el]
+                break
+            case 'en':
+                value = en[_string as keyof typeof en]
+                break
+            default:
+                break
+        }
+        return value
+    }
+   
+    log = (_string: string, data?: any) => {
+
+        let value = this.getCurrentlyLang(_string)
+
         if (data){
             for(let i = 0; i <= data.length; i++){
-                value = value.replace(`{#${i}}`, data[i-1])
+                value = value?.replace(`{#${i}}`, data[i-1])
             }
-        return  console.log(`${value}`)
 
         }
-        return console.log(en[_string as keyof typeof en])
+        return console.log(value)
     }
+
 }
+
 export const translate = Translate
 
 // let t = new Translate()
